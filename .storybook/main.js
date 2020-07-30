1e10b6442b0a0a6bac097de8dac55bedca5b2429
Clément Faure <dev@clement-faure.fr>
2020-07-30 11:01:42 +0200

    Fix storybook issues

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
+// https://github.com/storybookjs/storybook/issues/714
+// Soft link has been created on public directory
+
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.jsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: (config) => {
     config.resolve.alias = {
